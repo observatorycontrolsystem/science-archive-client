02b261970adc0e30b235d3a550e6fe939693c76a
Matt Daily <matt.daily85@gmail.com>
2021-06-01 14:01:34 -0700

    Add basic not found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h2>Oops! This looks like a black hole.</h2>
+      <p>Either you don't have permission to view this page or it doesn't exist.</p>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'NotFound'
+  }
+</script>
