5e09aaef9c7d9d328c6f2d7c1dced76b2ff07f26
Matt Daily <mdaily@lco.global>
2022-12-12 21:31:44 -0800

    First pass at working ds9 helper
    This is the basis for a helper application that uses a custom URI scheme to download and open images using DS9 given a set of frames from the archive frontend

---
+++ public/scripts/ds9_script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+function getJsonVal () {
+    python3 -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1))";
+}
+
+# make sure we add this to the path so we can access ds9
+PATH=$PATH:/usr/local/bin
+
+# split URI params into array
+arr=(${1//[=&]/ })
+
+# URI param values are the odd array elements, keys the even
+frames=${arr[1]}
+authtoken=${arr[3]}
+frame_url=${arr[5]}
+
+echo "Downloading frames to the /tmp/archive_download directory"
+mkdir /tmp/archive_download
+for f in $(echo $frames | tr "," "\n"); do
+    if [ "$authtoken" != "AUTHTOKEN" ]; then
+      json=$(wget -qO- --header="Authorization: Token $authtoken" "$frame_url$f/")
+    else
+      json=$(wget -qO- "$frame_url$f/")
+    fi
+    filename=$(echo $json| getJsonVal "['filename']" | tr -d '"')
+    url=$(echo $json | getJsonVal "['url']" | tr -d '"')
+    wget -O /tmp/archive_download/$filename -c $url
+done
+
+# now open these in ds9
+ds9 -zscale -lock frame wcs -view colorbar yes /tmp/archive_download/*
+
+# now remove the files
+rm -rf /tmp/archive_download
+
