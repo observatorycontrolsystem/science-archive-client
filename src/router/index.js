45c6c62a6cfdd3e03f5721de9b8363a7807fc993
Matt Daily <mdaily@lco.global>
2023-08-09 14:50:08 -0700

    If user is authenticated, set public=true
    The expectation is that users should be able to see all data they have access to, by default

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
       // otherwise send authenticated users to public=false
       // and unauthenticated users to public=true
       if (store.state.userIsAuthenticated) {
-        let query = {...to.query, public: "false"};
+        let query = {...to.query, public: "true"};
         // make sure we set the include_configuration_type correctly based on the DQI setting
         query.exclude_calibrations = store.state.inspectorViewEnabled ? false : true;
         next({ name: 'Home', query: query});
